38050ba5400f031697af72effecc45c9f0cd8b4d
John Kirk <jkirk777@gmail.com>
2012-10-23 23:39:25 -0600

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,39 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/samsung/victory/BoardConfigVendor.mk
+
+TARGET_ARCH_VARIANT := armv7-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+
+HAVE_HTC_AUDIO_DRIVER := true
+BOARD_USES_GENERIC_AUDIO := true
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := unknown
+TARGET_BOOTLOADER_BOARD_NAME := victory
+
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31
+BOARD_KERNEL_BASE := 0x80208000
+BOARD_FORCE_RAMDISK_ADDRESS := 0x81500000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/samsung/victory/kernel
+TARGET_KERNEL_CONFIG := gogh_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/victory/
+
+# Enable Bluetooth 
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+#BOARD_HAS_NO_SELECT_BUTTON := true
+# Use this flag if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
