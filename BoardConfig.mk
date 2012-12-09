157145b88466b290f97ea697a5699092d4eb1bad
John Kirk <john@asus-mint.(none)>
2012-12-09 13:06:19 -0700

    Added kernel base, ramdisk address and commandline. Edits to recovery.fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,17 +3,19 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/samsung/victory/BoardConfigVendor.mk
 
+# Arch
 TARGET_ARCH_VARIANT := armv7-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 
-HAVE_HTC_AUDIO_DRIVER := true
-BOARD_USES_GENERIC_AUDIO := true
+# Platform
+TARGET_BOARD_PLATFORM := msm8960
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
 TARGET_BOOTLOADER_BOARD_NAME := victory
 
+# Kernel
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_FORCE_RAMDISK_ADDRESS := 0x81500000
